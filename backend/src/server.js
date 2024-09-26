a067c830f2da105fc109537f21cc994e1ee738b7
Matthew Neba <mattneba4343@gmail.com>
2024-09-26 09:50:47 -0600

    Fix typo

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,7 +36,7 @@ const app = express();
 
 const corsOptions = {
 	origin: [
-		"http://pacplatform-frontend.apps.mastrmatt.com",
+		"http://pacplatform.apps.mastrmatt.com",
 		"http://localhost:5173",
 		"http://localhost:5174",
 		"http://localhost:5175",
