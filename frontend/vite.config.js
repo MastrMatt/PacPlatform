b96188672cc0f945e26741cf1358f2c724444eb4
Matthew Neba <mattneba4343@gmail.com>
2024-09-26 09:01:22 -0600

    Set up for local deployment on VPS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,14 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	server: {
+		host: "0.0.0.0", // This tells Vite to listen on all interfaces
+		port: 5173,
+	},
 });
